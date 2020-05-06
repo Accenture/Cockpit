7a11422cac8f24e1f3b44de37b618cbbcaf73f00
Zuming <zuming.liu@accenture.com>
2020-05-06 19:21:25 +0200

    refactor(dataModel): refactor data model for sprint team etc

--- cockpit-frontend/src/models/Technology.jsx
+++ cockpit-frontend/src/models/Technology.jsx
@@ -1,29 +1,15 @@
 import { Model, attr } from 'redux-orm';
-import { createSlice } from '@reduxjs/toolkit';
 
 export class Technology extends Model {
-  static modelName = 'Technology';
-
-  static get fields() {
-    return {
-      name: attr(),
-      logoUrl: attr(),
-    };
-  }
-
-  static slice = createSlice({
-    name: 'technoSlice',
-    initialState: undefined,
-    reducers: {
-      saveTechno(Technology, action) {
-        Technology.create(action.payload);
-      },
-    },
-  });
-
   toString() {
     return `Technology: ${this.name}`;
   }
 }
+Technology.modelName = 'Technology';
+
+Technology.fields = {
+  name: attr(),
+  logoUrl: attr(),
+};
+
 export default Technology;
-export const { saveTechno } = Technology.slice.actions;
