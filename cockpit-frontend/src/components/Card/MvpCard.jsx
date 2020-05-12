74c698ecccd22632a3826da7559899a6f6190e07
Kehl Taylor <kehltaylor@protonmail.com>
2020-05-12 11:35:44 +0200

    refactor(css): Changed css to js, fixed docker-compose issues, fixed home page issues

--- cockpit-frontend/src/components/Card/MvpCard.jsx
+++ cockpit-frontend/src/components/Card/MvpCard.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -7,30 +6,23 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import logo from '../../common/media/total-logo.jpg';
-import './MvpCard.scss';
-
-const useStyles = makeStyles((theme) => ({
-  cardRoot: {
-    minWidth: 325,
-    padding: theme.spacing(2),
-  },
-}));
+import useStyles from './styles';
 
 export default function MvpCard(props) {
   const classes = useStyles();
   const { mvpInfo } = props;
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={3}>
+    <Grid container spacing={1}>
+      <Grid item xs={2}>
         <Card className={classes.cardRoot}>
           <CardMedia
-            className="cardMedia"
+            className={classes.cardMedia}
             image={mvpInfo.mvpAvatarUrl ? mvpInfo.mvpAvatarUrl : logo}
-            title="Paella dish"
+            title={mvpInfo.name ? mvpInfo.name : "Unnamed MVP"}
           />
           <CardContent>
             <Typography
-              className="title"
+              className={classes.title}
               variant="h5"
               component="h2"
               gutterBottom
@@ -45,7 +37,7 @@ export default function MvpCard(props) {
                     : 'Unknown'}
                 </Typography>
                 <Typography
-                  className="title"
+                  className={classes.title}
                   color="textSecondary"
                   gutterBottom
                 >
@@ -57,7 +49,7 @@ export default function MvpCard(props) {
                   {mvpInfo.currentSprint ? mvpInfo.currentSprint : 'Unknown'}
                 </Typography>
                 <Typography
-                  className="title"
+                  className={classes.title}
                   color="textSecondary"
                   gutterBottom
                 >
@@ -70,11 +62,11 @@ export default function MvpCard(props) {
                 <Typography gutterBottom>Mood</Typography>
                 <div>
                   <LinearProgress
-                    className="progress"
+                    className={classes.progress}
                     variant="determinate"
                     value={75}
                   />
-                  <span className="progressBarTxt" style={{ left: '60%' }}>
+                  <span className={classes.progressBarTxt} style={{ left: '60%' }}>
                     3
                   </span>
                 </div>
@@ -83,11 +75,11 @@ export default function MvpCard(props) {
                 <Typography gutterBottom>Confidence</Typography>
                 <div>
                   <LinearProgress
-                    className="progress"
+                    className={classes.progress}
                     variant="determinate"
                     value={25}
                   />
-                  <span className="progressBarTxt" style={{ left: '10%' }}>
+                  <span className={classes.progressBarTxt} style={{ left: '10%' }}>
                     1
                   </span>
                 </div>
@@ -96,11 +88,11 @@ export default function MvpCard(props) {
                 <Typography gutterBottom>Motivation</Typography>
                 <div>
                   <LinearProgress
-                    className="progress"
+                    className={classes.progress}
                     variant="determinate"
                     value={50}
                   />
-                  <span className="progressBarTxt" style={{ left: '35%' }}>
+                  <span className={classes.progressBarTxt} style={{ left: '35%' }}>
                     2
                   </span>
                 </div>
--- cockpit-frontend/src/components/HomePage/HomePage.jsx
+++ cockpit-frontend/src/components/HomePage/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from '../Header/Header';
 import MvpTabs from '../MvpTabPanel/MvpTabPanel';
-import './HomePage.scss';
 
 function HomePage() {
   return (
