32c3c1e9aa7389cd60e96b2f62784a3221c24094
Kehl Taylor <kehltaylor@protonmail.com>
2020-05-19 19:17:21 +0200

    refactor(backend): Added data migration, added flyway, created models, mapped models

--- cockpit_api/src/main/java/com/cockpit/api/CockpitApplication.java
+++ cockpit_api/src/main/java/com/cockpit/api/CockpitApplication.java
@@ -2,8 +2,10 @@ package com.cockpit.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class CockpitApplication {
 
 	public static void main(String[] args) {
---
+++ cockpit_api/src/main/java/com/cockpit/api/Jira.java
@@ -0,0 +1,63 @@
+package com.cockpit.api;
+
+import javax.persistence.*;
+
+@Entity
+public class Jira extends EntityWithUUID{
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String jiraProjectKey;
+    private int currentSprint;
+    private int jiraProjectId;
+    private String mvpStartDate;
+    private String mvpEndDate;
+    @OneToOne(mappedBy = "jira")
+    private Mvp mvp;
+
+    public String getJiraProjectKey() {
+        return jiraProjectKey;
+    }
+
+    public int getCurrentSprint() {
+        return currentSprint;
+    }
+
+    public int getJiraProjectId() {
+        return jiraProjectId;
+    }
+
+    public String getMvpStartDate() {
+        return mvpStartDate;
+    }
+
+    public String getMvpEndDate() {
+        return mvpEndDate;
+    }
+
+    public Mvp getMvp() {
+        return mvp;
+    }
+
+    public void setJiraProjectKey(String jiraProjectKey) {
+        this.jiraProjectKey = jiraProjectKey;
+    }
+
+    public void setCurrentSprint(int currentSprint) {
+        this.currentSprint = currentSprint;
+    }
+
+    public void setJiraProjectId(int jiraProjectId) {
+        this.jiraProjectId = jiraProjectId;
+    }
+
+    public void setMvpStartDate(String mvpStartDate) {
+        this.mvpStartDate = mvpStartDate;
+    }
+
+    public void setMvpEndDate(String mvpEndDate) {
+        this.mvpEndDate = mvpEndDate;
+    }
+
+    public void setMvp(Mvp mvp) {
+        this.mvp = mvp;
+    }
+}
