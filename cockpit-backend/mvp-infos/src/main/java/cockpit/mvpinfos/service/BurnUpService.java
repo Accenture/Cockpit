4674ef309d0a2008dadc4ddf9c79efc26439b4c1
rihab <rihabrjab08@gmail.com>
2020-05-28 10:24:32 +0200

    fix(burn-up): fix backend burn-up service

--- cockpit-backend/mvp-infos/src/main/java/cockpit/mvpinfos/service/BurnUpService.java
+++ cockpit-backend/mvp-infos/src/main/java/cockpit/mvpinfos/service/BurnUpService.java
@@ -29,52 +29,46 @@ public class BurnUpService {
 	public List<ChartData> getChartData(String jiraProjectKey) {
 		List<ChartData> chartDataList = new ArrayList<>();
 		double projection = 0;
-		int previousTotalStories = 0;
 		int actualSprintStories = 0;
 		int totalUSNumber = 0;
-		int taux = 0;
-		long lastNbUsClosed=0;
-		int iteration=1;
+		double taux = 0.0;
+		long lastNbUsClosed = 0;
+		int iteration = 1;
 		Mvp mvp = mvpRepository.findById(jiraProjectKey).orElse(null);
 		for (int sprintNumber = 0; sprintNumber < mvp.getNbSprint(); sprintNumber++) {
 			ChartData chartData = new ChartData();
 			Long numberOfUsClosed = userStoryService.findSumOfUsClosedForSprint(mvp, sprintNumber);
 
-			// If sprint is before Today, we display it in GREEN in Chart
 			if (sprintNumber < sprintService.findSprintNumberForADate(mvp, Calendar.getInstance().getTime())) {
 				chartData.setUsClosed(numberOfUsClosed);
-				// If date is today, Then we add both data to have a continuous
-				// line in the chart
+
 			} else if (sprintNumber == sprintService.findSprintNumberForADate(mvp, Calendar.getInstance().getTime())) {
 				chartData.setUsClosed(numberOfUsClosed);
-				// chartData.setProjectionUsClosed(numberOfUsClosed);
-				// projection = numberOfUsClosed;
 			}
-			// else {
-			// Else we calculate all sprints velocity for projection
+
 			if (chartData.getUsClosed() != null) {
-				taux = (int) (numberOfUsClosed / (sprintNumber + 1));
-				lastNbUsClosed=numberOfUsClosed;
+				taux = (numberOfUsClosed / (sprintNumber + 1));
+				lastNbUsClosed = numberOfUsClosed;
 			}
 			if (chartData.getUsClosed() == null) {
-				projection =  numberOfUsClosed +iteration*taux;
+				projection = numberOfUsClosed + iteration * taux;
 				chartData.setProjectionUsClosed(projection);
 				iteration++;
-				
-		}
-			// }
+			}
 
 			// Calcul code
 			chartData.setSprintId(sprintNumber);
 			chartData.setTotalSprints(mvp.getNbSprint());
 			actualSprintStories = getTotalNumberOfStoriesForAnMVPAndASprintNumber(sprintNumber, mvp);
-			if (actualSprintStories == 0) {
-				actualSprintStories = previousTotalStories;
-			} else {
-				previousTotalStories = actualSprintStories;
-			}
+		
 			totalUSNumber = totalUSNumber + actualSprintStories;
-			chartData.setExpectedUsClosed(totalUSNumber);
+			if (actualSprintStories != 0) {
+				chartData.setExpectedUsClosed(totalUSNumber);
+
+			} else {
+				chartData.setExpectedUsClosed(null);
+			}
+
 			Sprint currentSprint = sprintRepository.findByMvpAndSprintNumber(mvp, sprintNumber);
 			if (currentSprint != null && currentSprint.getTotalNbUs() != null) {
 				chartData.setTotalStories(currentSprint.getTotalNbUs());
@@ -85,7 +79,7 @@ public class BurnUpService {
 			if (chart.getUsClosed() == null) {
 				chartDataList.get(chartDataList.indexOf(chart) - 1).setProjectionUsClosed(
 						(double) chartDataList.get(chartDataList.indexOf(chart) - 1).getUsClosed());
-				
+
 				break;
 			}
 		return chartDataList;
