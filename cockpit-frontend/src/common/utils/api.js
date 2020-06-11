cd94beb65d435975a6626ecd2647b310c2d22744
Zuming <zuming.liu@accenture.com>
2020-06-11 16:20:48 +0200

    feat(param): set different backend server url for different envs

--- cockpit-frontend/src/common/utils/api.js
+++ cockpit-frontend/src/common/utils/api.js
@@ -1,6 +1,20 @@
 import axios from 'axios';
 
+const env = process.env.REACT_APP_STAGE;
+let backendUrl;
+
+switch (env) {
+  case 'local':
+    backendUrl = 'http://localhost:8085';
+    break;
+  case 'dev':
+    backendUrl = 'https://cockpit-backend-dev.azurewebsites.net';
+    break;
+  default:
+    backendUrl = 'https://cockpit-backend-dev.azurewebsites.net';
+}
+
 export default axios.create({
-  baseURL: 'http://localhost:8085/api/1.0/',
+  baseURL: `${backendUrl}/api/v1`,
   responseType: 'json',
 });
