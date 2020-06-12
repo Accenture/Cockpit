5b5e06e46967c1e1850f8969f2577ae285bbf3e3
Zuming <zuming.liu@accenture.com>
2020-06-12 14:23:52 +0200

    feat(cross-origin): add cross origin for all endpoints

--- cockpit_api/src/main/java/com/cockpit/api/controller/UserStoryController.java
+++ cockpit_api/src/main/java/com/cockpit/api/controller/UserStoryController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-//@CrossOrigin
+@CrossOrigin
 public class UserStoryController {
     private final UserStoryService userStoryService;
 
