18630e3f5443a94fd0a74a5c8d23dc48d2d37ac4
rihab <rihabrjab08@gmail.com>
2020-06-14 22:18:20 +0200

    feat(edit-mvp): add edit mvp informations form

---
+++ cockpit-frontend/src/components/InformationForm/styles.js
@@ -0,0 +1,28 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  paper: {
+    maxHeight: 400,
+    overflow: 'auto',
+    width: 600,
+    borderRadius: 0,
+  },
+  grid: {
+    padding: theme.spacing(2, 4, 1),
+    width: 'calc(80%)',
+    margin: 0,
+  },
+  formLabel: {
+    fontWeight: 'bold',
+  },
+  textField: {
+    marginTop: 8,
+    'margin-bottom': 8,
+  },
+  imgStyle: {
+    height: 50,
+    width: 50,
+    padding: theme.spacing(0, 0, 0, 1),
+    marginTop: 25,
+  },
+}));
