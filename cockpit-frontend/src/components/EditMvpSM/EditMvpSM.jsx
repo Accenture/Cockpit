92948f37eca7a72c9cd60950cd3ef7ce6700d0b6
rihab <rihabrjab08@gmail.com>
2020-06-14 22:18:20 +0200

    feat(edit-mvp): add edit mvp informations form

--- cockpit-frontend/src/components/EditMvpSM/EditMvpSM.jsx
+++ cockpit-frontend/src/components/EditMvpSM/EditMvpSM.jsx
@@ -1,48 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import InformationForm from '../InformationForm/InformationForm';
+
 import { editSMFormState, closeEditMvpSMForm } from '../Header/HeaderSlice';
+import useStyles from './styles';
 
 export default function EditMvpSMForm() {
+  const [value, setValue] = useState(0);
+
+  const classes = useStyles();
   const dispatch = useDispatch();
   const open = useSelector(editSMFormState);
   const handleClose = () => {
     dispatch(closeEditMvpSMForm());
   };
+  const handleChange = (event, valeur) => {
+    setValue(valeur);
+  };
   const body = (
     <div>
       <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          <ButtonGroup color="primary" orientation="vertical">
-            <Button variant="contained">Overview</Button>
-            <Button disabled>Sprint 0</Button>
-            <Button disabled>Sprint 1</Button>
-            <Button disabled>Sprint 2</Button>
-            <Button disabled>Sprint 3</Button>
-            <Button disabled>Sprint 4</Button>
-            <Button disabled>Sprint 5</Button>
-            <Button disabled>Sprint 6</Button>
+        <div className={classes.root}>
+          <ButtonGroup
+            color="primary"
+            orientation="vertical"
+            className={classes.ButtonGroup}
+          >
+            <Button className={classes.buttonStyle} variant="contained">
+              Overview
+            </Button>
+            <Button className={classes.buttonStyle} disabled>
+              Sprint 0
+            </Button>
+            <Button className={classes.buttonStyle} disabled>
+              Sprint 1
+            </Button>
+            <Button className={classes.buttonStyle} disabled>
+              Sprint 2
+            </Button>
+            <Button className={classes.buttonStyle} disabled>
+              Sprint 3
+            </Button>
+            <Button className={classes.buttonStyle} disabled>
+              Sprint 4
+            </Button>
+            <Button className={classes.buttonStyle} disabled>
+              Sprint 5
+            </Button>
+            <Button className={classes.buttonStyle} disabled>
+              Sprint 6
+            </Button>
+            <Button className={classes.buttonStyle} disabled>
+              Sprint 7
+            </Button>
           </ButtonGroup>
-        </DialogContentText>
+          <div>
+            <Tabs
+              indicatorColor="primary"
+              textColor="primary"
+              centered
+              value={value}
+              onChange={handleChange}
+            >
+              <Tab label="Information" />
+              <Tab label="Team" />
+              <Tab label="Technologies" />
+            </Tabs>
+            {value === 0 && <InformationForm />}
+            {value === 1 && <div>Teams</div>}
+            {value === 2 && <div>Technologies</div>}
+          </div>
+        </div>
       </DialogContent>
       <DialogActions>
-        {/* <Button onClick={handleClose} color="primary">
-          Disagree
+        <Button onClick={handleClose} style={{ borderRadius: 20 }}>
+          Cancel
         </Button>
-        <Button onClick={handleClose} color="primary" autoFocus>
-          Agree
-  </Button> */}
+        <Button
+          onClick={handleClose}
+          color="primary"
+          variant="contained"
+          autoFocus
+          style={{ borderRadius: 20 }}
+        >
+          Save
+        </Button>
       </DialogActions>
     </div>
   );
   return (
     <div>
       <Dialog
+        maxWidth="xl"
+        className={classes.modal}
         open={open}
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
---
+++ cockpit-frontend/src/components/InformationForm/InformationForm.jsx
@@ -0,0 +1,183 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import FormLabel from '@material-ui/core/FormLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import Paper from '@material-ui/core/Paper';
+import FormControl from '@material-ui/core/FormControl';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { mvpSelector } from '../../redux/selector';
+import useStyles from './styles';
+
+export default function InformationForm() {
+  const classes = useStyles();
+
+  const mvpId = useParams().id;
+  const mvp = useSelector((state) => mvpSelector(state, mvpId));
+
+  return (
+    <Paper className={classes.paper}>
+      <form>
+        <Grid className={classes.grid} container spacing={1}>
+          <Grid item xs={12}>
+            <FormLabel className={classes.formLabel}>Name</FormLabel>
+            <TextField
+              className={classes.textField}
+              required
+              fullWidth
+              value={mvp.name ? mvp.name : 'Unnamed MVP'}
+              variant="outlined"
+              id="mvpName"
+              name="mvpName"
+              placeholder="MVP Name"
+              autoComplete="mvpName"
+              size="small"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormLabel className={classes.formLabel}>Cycle</FormLabel>
+            <TextField
+              className={classes.textField}
+              value={mvp.iterationNumber}
+              required
+              fullWidth
+              variant="outlined"
+              id="cycle"
+              name="cycle"
+              placeholder="Cycle"
+              autoComplete="cycle"
+              size="small"
+              type="number"
+              inputProps={{ min: '1', step: '1' }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormLabel className={classes.formLabel}>Status</FormLabel>
+            <FormControl
+              required
+              size="small"
+              fullWidth
+              variant="outlined"
+              className={classes.textField}
+              value={mvp.status}
+            >
+              <Select displayEmpty value={mvp.status || ''}>
+                <MenuItem value="condidates">Condidates</MenuItem>
+                <MenuItem value="inprogress">In progress</MenuItem>
+                <MenuItem value="transferred">Transferred</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <FormLabel className={classes.formLabel}>Entity</FormLabel>
+            <FormControl
+              required
+              size="small"
+              fullWidth
+              variant="outlined"
+              className={classes.textField}
+            >
+              <Select displayEmpty value={mvp.entity || ''}>
+                <MenuItem value="EP">EP</MenuItem>
+                <MenuItem value="RC">RC</MenuItem>
+                <MenuItem value="MS">MS</MenuItem>
+                <MenuItem value="GRP">GRP</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <div style={{ display: 'flex' }}>
+              <div>
+                <FormLabel className={classes.formLabel}>
+                  MVP photo URL
+                </FormLabel>
+                <TextField
+                  className={classes.textField}
+                  value={mvp.mvpAvatarUrl}
+                  required
+                  fullWidth
+                  variant="outlined"
+                  id="mvpPhoto"
+                  name="mvpPhoto"
+                  placeholder="http://"
+                  autoComplete="mvpPhoto"
+                  size="small"
+                />
+              </div>
+              <img
+                className={classes.imgStyle}
+                src={mvp.mvpAvatarUrl}
+                alt="img"
+              />
+            </div>
+          </Grid>
+          <Grid item xs={12}>
+            <FormLabel className={classes.formLabel}>Pitch</FormLabel>
+            <TextField
+              className={classes.textField}
+              value={mvp.pitch || ''}
+              required
+              fullWidth
+              variant="outlined"
+              id="pitch"
+              name="pitch"
+              placeholder="Pitch"
+              autoComplete="pitch"
+              size="small"
+              multiline
+              rows="3"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormLabel className={classes.formLabel}>Sprint number</FormLabel>
+            <TextField
+              className={classes.textField}
+              value={mvp.nbSprint}
+              required
+              fullWidth
+              variant="outlined"
+              id="sprintNumber"
+              name="sprintNumber"
+              placeholder="Sprint number"
+              autoComplete="sprintNumber"
+              size="small"
+              type="number"
+              inputProps={{ min: '0', step: '1' }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              id="date"
+              label="MVP start date"
+              type="date"
+              // defaultValue="2020-02-24"
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              value={mvp.mvpStartDate}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              id="date"
+              label="MVP end date"
+              type="date"
+              format="DD/MM/YYYY"
+              // defaultValue="2020-05-24"
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+              value={mvp.mvpEndDate}
+            />
+          </Grid>
+        </Grid>
+      </form>
+    </Paper>
+  );
+}
