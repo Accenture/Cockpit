46815ae8e300f9fef36c7b5b17f66f930d03e45b
rihab <rihabrjab08@gmail.com>
2020-06-16 10:28:25 +0200

    feat(edit-mvp): add submit function

--- cockpit-frontend/src/components/InformationForm/InformationForm.jsx
+++ cockpit-frontend/src/components/InformationForm/InformationForm.jsx
@@ -8,42 +8,95 @@ import Paper from '@material-ui/core/Paper';
 import FormControl from '@material-ui/core/FormControl';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import MvpService from '../../services/service';
 import { mvpSelector } from '../../redux/selector';
 import useStyles from './styles';
 
 export default function InformationForm() {
   const classes = useStyles();
-  const [mvp, setMvp] = useState({
-    id: null,
-    name: 'mvpName',
-    pitch: 'Hello',
-    status: 'inprogress',
-    iterationNumber: 1,
-    entity: 'es',
-    currentSprint: 2,
-    nbSprint: 0,
-    mvpEndDate: '',
-    mvpStartDate: '',
-    mvpAvatarUrl: 'imageUrl',
-    jiraProjectId: 10009,
-    jiraBoardId: 1004,
-    location: '',
-    nbUsersStories: 0,
-    bugsCount: 0,
-    allBugsCount: 0,
-    timeToFix: '',
-    timeToDetect: '',
-    cascade: [{}],
-    sprint: [{}],
-    userStories: [{}],
-    bugHistories: [{}],
-  });
+
+  const [name, setName] = useState('');
+  const [pitch, setPitch] = useState('');
+  const [cycle, setCycle] = useState(0);
+  const [status, setStatus] = useState('');
+  const [entity, setEntity] = useState('');
+  const [urlMvpAvatar, setUrlMvpAvatar] = useState('');
+  const [nbSprint, setNbSprint] = useState(8);
+  const [mvpStartDate, setMvpStartDate] = useState(0);
+  const [mvpEndDate, setMvpEndDate] = useState(0);
   const mvpId = useParams().id;
   const mvpInfo = useSelector((state) => mvpSelector(state, mvpId));
   useEffect(() => {
-    setMvp(mvpInfo);
+    setName(mvpInfo.name);
+    setPitch(mvpInfo.pitch);
+    setCycle(mvpInfo.cycle);
+    setStatus(mvpInfo.status);
+    setEntity(mvpInfo.entity);
+    setUrlMvpAvatar(mvpInfo.urlMvpAvatar);
+    // setNbSprint(mvpInfo.nbSprint);
+    setMvpStartDate(mvpInfo.jira.mvpStartDate);
+    setMvpEndDate(mvpInfo.jira.mvpEndDate);
   }, [mvpInfo]);
-
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+  function handlePitchChange(event) {
+    setPitch(event.target.value);
+  }
+  function handleCycleChange(event) {
+    setCycle(event.target.value);
+  }
+  function handleStatusChange(event) {
+    setStatus(event.target.value);
+  }
+  function handleEntityChange(event) {
+    setEntity(event.target.value);
+  }
+  function handleImageChange(event) {
+    setUrlMvpAvatar(event.target.value);
+  }
+  function handleStartDateChange(event) {
+    setMvpStartDate(event.target.value);
+  }
+  function handleEndDateChange(event) {
+    setMvpEndDate(event.target.value);
+  }
+  async function submit(e) {
+    e.preventDefault();
+    const newJira = {
+      jiraProjectKey: mvpInfo.jira.jiraProjectKey,
+      currentSprint: mvpInfo.jira.currentSprint,
+      jiraProjectId: mvpInfo.jira.jiraProjectId,
+      mvpStartDate: mvpStartDate,
+      mvpEndDate: mvpEndDate,
+      mvp: {
+        name: name,
+        entity: entity,
+        urlMvpAvatar: urlMvpAvatar,
+        cycle: cycle,
+        mvpDescription: pitch,
+        status: status,
+        team: {
+          name: '',
+          teamMembers: [
+            {
+              firstName: '',
+              lastName: '',
+              email: '',
+            },
+          ],
+        },
+        technologies: [
+          {
+            name: '',
+            url: '',
+          },
+        ],
+      },
+    };
+    await MvpService.updateJiraProject(newJira);
+   // dispatch(close());
+  }
   return (
     <Paper className={classes.paper}>
       <form>
@@ -54,19 +107,20 @@ export default function InformationForm() {
               className={classes.textField}
               required
               fullWidth
-              value={mvpInfo.name ? mvpInfo.name : ''}
+              value={name}
               variant="outlined"
               id="mvpName"
-              name="mvpName"
+              name="name"
               placeholder="MVP Name"
               size="small"
+              onChange={handleNameChange}
             />
           </Grid>
           <Grid item xs={12}>
             <FormLabel className={classes.formLabel}>Cycle</FormLabel>
             <TextField
               className={classes.textField}
-              value={mvp.iterationNumber}
+              value={cycle}
               required
               fullWidth
               variant="outlined"
@@ -76,6 +130,7 @@ export default function InformationForm() {
               size="small"
               type="number"
               inputProps={{ min: '1', step: '1' }}
+              onChange={handleCycleChange}
             />
           </Grid>
           <Grid item xs={12}>
@@ -86,9 +141,13 @@ export default function InformationForm() {
               fullWidth
               variant="outlined"
               className={classes.textField}
-              value={mvp.status}
+              value={mvpInfo.status}
             >
-              <Select displayEmpty value={mvp.status || ''}>
+              <Select
+                displayEmpty
+                value={status || ''}
+                onChange={handleStatusChange}
+              >
                 <MenuItem value="condidates">Condidates</MenuItem>
                 <MenuItem value="inprogress">In progress</MenuItem>
                 <MenuItem value="transferred">Transferred</MenuItem>
@@ -104,7 +163,11 @@ export default function InformationForm() {
               variant="outlined"
               className={classes.textField}
             >
-              <Select displayEmpty value={mvp.entity || ''}>
+              <Select
+                displayEmpty
+                value={entity || ''}
+                onChange={handleEntityChange}
+              >
                 <MenuItem value="EP">EP</MenuItem>
                 <MenuItem value="RC">RC</MenuItem>
                 <MenuItem value="MS">MS</MenuItem>
@@ -120,7 +183,7 @@ export default function InformationForm() {
                 </FormLabel>
                 <TextField
                   className={classes.textField}
-                  value={mvp.mvpAvatarUrl}
+                  value={urlMvpAvatar}
                   required
                   fullWidth
                   variant="outlined"
@@ -128,20 +191,17 @@ export default function InformationForm() {
                   name="mvpPhoto"
                   placeholder="http://"
                   size="small"
+                  onChange={handleImageChange}
                 />
               </div>
-              <img
-                className={classes.imgStyle}
-                src={mvp.mvpAvatarUrl}
-                alt="img"
-              />
+              <img className={classes.imgStyle} src={urlMvpAvatar} alt="img" />
             </div>
           </Grid>
           <Grid item xs={12}>
             <FormLabel className={classes.formLabel}>Pitch</FormLabel>
             <TextField
               className={classes.textField}
-              value={mvp.pitch || ''}
+              value={pitch || ''}
               required
               fullWidth
               variant="outlined"
@@ -151,13 +211,14 @@ export default function InformationForm() {
               size="small"
               multiline
               rows="3"
+              onChange={handlePitchChange}
             />
           </Grid>
           <Grid item xs={12}>
             <FormLabel className={classes.formLabel}>Sprint number</FormLabel>
             <TextField
               className={classes.textField}
-              value={mvp.nbSprint}
+              value={nbSprint}
               required
               fullWidth
               variant="outlined"
@@ -180,7 +241,8 @@ export default function InformationForm() {
                 shrink: true,
               }}
               variant="outlined"
-              value={mvp.mvpStartDate || ''}
+              value={mvpStartDate || ''}
+              onChange={handleStartDateChange}
             />
           </Grid>
           <Grid item xs={6}>
@@ -195,7 +257,8 @@ export default function InformationForm() {
                 shrink: true,
               }}
               variant="outlined"
-              value={mvp.mvpEndDate || ''}
+              value={mvpEndDate || ''}
+              onChange={handleEndDateChange}
             />
           </Grid>
         </Grid>
