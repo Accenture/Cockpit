43041e6ac022126a7dae642a2699ac9aaec4bb20
Zuming <zuming.liu@accenture.com>
2020-06-19 12:39:45 +0200

    feat(state-persistence): replace local storage with  session storage

--- cockpit-frontend/src/redux/store.js
+++ cockpit-frontend/src/redux/store.js
@@ -5,7 +5,7 @@ import {
 } from '@reduxjs/toolkit';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import storage from 'redux-persist/lib/storage/session';
 import mvnMenuReducer from '../components/MvpMenu/mvpMenuSlice';
 import ormReducer from './ormSlice';
 import headerReducer from '../components/Header/HeaderSlice';
