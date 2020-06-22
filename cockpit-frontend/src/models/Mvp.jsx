9c0a1466e2375e0ad7816c2202619952a4c5097e
Zuming <zuming.liu@accenture.com>
2020-06-22 16:21:25 +0200

    refactor(orm): remove unnecessary orm models

--- cockpit-frontend/src/models/Mvp.jsx
+++ cockpit-frontend/src/models/Mvp.jsx
@@ -7,26 +7,4 @@ export class Mvp extends Model {
 }
 Mvp.modelName = 'Mvp';
 
-/*
-Mvp.fields = {
-  id: attr(),
-  name: attr(),
-  entity: attr(),
-  urlMvpAvatar: attr(),
-  cycle: attr(),
-  mvpDescription: attr(),
-  status: attr(),
-  team: fk({
-    to: 'Team',
-    as: 'team',
-    relatedName: 'mvps',
-  }),
-  technologies: many({
-    to: 'Technology',
-    as: 'technology',
-    relatedName: 'mvps',
-  }),
-};
-*/
-
 export default Mvp;
