51c5a0b786b3de438a295642d3ab79f7ea3ed740
rihab <rihabrjab08@gmail.com>
2020-06-25 17:05:21 +0200

    fix(junit-test): fix mock function in controller

--- cockpit_api/src/test/java/com/cockpit/api/controller/MvpControllerTest.java
+++ cockpit_api/src/test/java/com/cockpit/api/controller/MvpControllerTest.java
@@ -3,11 +3,11 @@ package com.cockpit.api.controller;
 import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -22,23 +22,21 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.cockpit.api.model.dao.Mvp;
 import com.cockpit.api.model.dao.Team;
-import com.cockpit.api.repository.MvpRepository;
-import com.cockpit.api.repository.TeamRepository;
+import com.cockpit.api.model.dto.MvpDTO;
+
 import com.cockpit.api.service.MvpService;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = { MvpController.class, MvpService.class })
+@ContextConfiguration(classes = { MvpController.class })
 @WebMvcTest
 public class MvpControllerTest {
+	private ModelMapper modelMapper = new ModelMapper();
 
 	@Autowired
 	private MockMvc mockMvc;
 
 	@MockBean
-	private MvpRepository mvpRepository;
-
-	@MockBean
-	private TeamRepository teamRepository;
+	private MvpService mvpService;
 
 	@Test
 	public void WhenUnassignTeamOfMvp_thenReturn200() throws Exception {
@@ -48,17 +46,17 @@ public class MvpControllerTest {
 		mockMvp.setId(1l);
 		mockTeam.setName("team1");
 		mockMvp.setTeam(mockTeam);
-		Optional<Mvp> mvp = Optional.ofNullable(mockMvp);
+		MvpDTO mvpDto = modelMapper.map(mockMvp, MvpDTO.class);
 
 		// given
-		  Mockito.when(mvpRepository.findById(mockMvp.getId())).thenReturn(mvp);
-		  Mockito.when(mvpRepository.save(mockMvp)).thenReturn(mockMvp);
+		Mockito.when(mvpService.unassignTeamOfMvp(mockMvp.getId())).thenReturn(mvpDto);
+		
 		// when
-		MvcResult result = mockMvc.perform(
-				MockMvcRequestBuilders.put("/api/v1/mvp/unassignTeam/{id}", mockMvp.getId()).accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk()).andReturn();
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/mvp/unassignTeam/{id}", mockMvp.getId())
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
 
 		MockHttpServletResponse response = result.getResponse();
+		
 		// then
 		assertEquals(HttpStatus.OK.value(), response.getStatus());
 	}
