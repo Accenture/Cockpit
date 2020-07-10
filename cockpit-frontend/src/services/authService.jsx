a8b6a263bd422a95544ba5c8efc35ac938707702
Zuming <zuming.liu@accenture.com>
2020-07-10 18:43:07 +0200

    refactor(auth): remove logs

--- cockpit-frontend/src/services/authService.jsx
+++ cockpit-frontend/src/services/authService.jsx
@@ -1,5 +1,4 @@
 import { UserManager, WebStorageStateStore, Log } from 'oidc-client';
-import { useHistory } from 'react-router-dom';
 import authConfig from '../common/utils/authConfig';
 
 export default class AuthService {
@@ -37,6 +36,7 @@ export default class AuthService {
   getUser = async () => {
     const user = await this.UserManager.getUser();
     if (!user) {
+      // eslint-disable-next-line no-return-await
       return await this.UserManager.signinRedirectCallback();
     }
     return user;
@@ -59,24 +59,14 @@ export default class AuthService {
         `oidc.user:https://external-total.okta.com/oauth2/default:0oajesx698Xd6LGCi4x6`,
       ),
     );
-    console.log(!!oidcStorage);
-    //console.log(!!oidcStorage.access_token);
     return !!oidcStorage && !!oidcStorage.access_token;
   };
 
   navigateToHomePage = () => {
-    console.log('6');
-
-    /*
-    const history = useHistory();
-    const path = '/';
-    console.log('Here 3');
-    history.push(path); */
     window.location.replace('/');
   };
 
   signinSilent = () => {
-    console.log('7');
     this.UserManager.signinSilent()
       .then((user) => {
         console.log('signed in', user);
@@ -87,17 +77,14 @@ export default class AuthService {
   };
 
   signinSilentCallback = () => {
-    console.log('8');
     this.UserManager.signinSilentCallback();
   };
 
   createSigninRequest = () => {
-    console.log('9');
     return this.UserManager.createSigninRequest();
   };
 
   logout = () => {
-    console.log('10');
     this.UserManager.signoutRedirect({
       id_token_hint: localStorage.getItem('id_token'),
     });
@@ -105,7 +92,6 @@ export default class AuthService {
   };
 
   signoutRedirectCallback = () => {
-    console.log('11');
     this.UserManager.signoutRedirectCallback().then(() => {
       localStorage.clear();
       window.location.replace(process.env.REACT_APP_PUBLIC_URL);
