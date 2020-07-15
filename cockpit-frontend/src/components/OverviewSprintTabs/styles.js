7f87db33b0fff76ebe2ef766e36d3aef75e416bf
rihab <rihabrjab08@gmail.com>
2020-07-15 09:55:50 +0200

    feat(sprint-details): add sprint selector

--- cockpit-frontend/src/components/OverviewSprintTabs/styles.js
+++ cockpit-frontend/src/components/OverviewSprintTabs/styles.js
@@ -1,8 +1,17 @@
 import { makeStyles } from '@material-ui/core/styles';
+import { lightGrey } from '../../common/scss/colorVarialble.scss';
 
 export default makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
+  },
+  formControl: {
+    position: 'absolute',
+    top: 0,
+    marginTop: '96px',
+    marginLeft: '190px',
+    backgroundColor: lightGrey,
+    padding: 4,
   },
 }));
