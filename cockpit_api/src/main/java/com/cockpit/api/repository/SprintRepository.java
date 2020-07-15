2f60ee63d972c0f8508fd3e3d3566a7236a6f320
Zuming <zuming.liu@accenture.com>
2020-07-15 11:10:57 +0200

    fix(bug):fix bugs

--- cockpit_api/src/main/java/com/cockpit/api/repository/SprintRepository.java
+++ cockpit_api/src/main/java/com/cockpit/api/repository/SprintRepository.java
@@ -6,14 +6,15 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SprintRepository extends CrudRepository<Sprint, Long> {
 
-	Sprint findTopBySprintStartDateLessThanEqualAndJiraEqualsOrderBySprintNumberDesc(Date date, Jira jira);
     Sprint findByJiraAndSprintNumber(Jira jira, int sprintNumber);
     Sprint findByJiraSprintId(int jiraSprintId);
     List<Sprint> findByJiraOrderBySprintNumber(Jira jira);
+    Sprint findTopBySprintStartDateLessThanEqualAndJiraEqualsOrderBySprintNumberDesc(@Param("date") Date date, @Param("jira") Jira jira);
 
 }
