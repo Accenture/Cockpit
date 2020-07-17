e06e1a7bbe52c51a003800c0f49052aeef300900
rihab <rihabrjab08@gmail.com>
2020-07-17 11:57:47 +0200

    fix(update-burnup): fix popover style

---
+++ cockpit-frontend/src/components/BurnUpChart/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  popover: {
+    color: 'gray',
+    fontSize: 12,
+    fontStyle: 'italic',
+    padding: 4,
+  },
+}));
