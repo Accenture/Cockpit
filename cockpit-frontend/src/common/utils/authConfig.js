734f99f0bb4c9cce9560c3238590fd87f3b1090b
Zuming <zuming.liu@accenture.com>
2020-07-20 14:11:34 +0200

    fix(env): add env varialbles for qa

--- cockpit-frontend/src/common/utils/authConfig.js
+++ cockpit-frontend/src/common/utils/authConfig.js
@@ -1,5 +1,5 @@
 const AUTH_CONFIG = {
-  preprod: {
+  dev: {
     client_id: 'cockpit-dev',
     redirect_uri: 'https://azwbdcokp01.azurewebsites.net/authentication',
     response_type: 'id_token token',
@@ -20,8 +20,29 @@ const AUTH_CONFIG = {
         'https://pp-api-digitalpassport.hubtotal.net/sso/oauth2/total/connect/jwk_uri',
     },
   },
+  qa: {
+    client_id: 'cockpit-qa',
+    redirect_uri: 'https://azwbqcokp01.azurewebsites.net/authentication',
+    response_type: 'id_token token',
+    scope: 'openid email',
+    load_user_info: false,
+    authority: 'https://pp-sso-digitalpassport.hubtotal.net/sso/oauth2/total',
+    metadata: {
+      issuer: 'https://pp-sso-digitalpassport.hubtotal.net/sso',
+      end_session_endpoint:
+        'https://pp-sso-digitalpassport.hubtotal.net/sso/oauth2/total/connect/endSession',
+      check_session_iframe:
+        'https://pp-sso-digitalpassport.hubtotal.net/sso/oauth2/total/connect/checkSession',
+      authorization_endpoint:
+        'https://pp-sso-digitalpassport.hubtotal.net/sso/oauth2/total/authorize?usesso=true',
+      userinfo_endpoint:
+        'https://pp-api-digitalpassport.hubtotal.net/sso/oauth2/total/userinfo',
+      jwks_uri:
+        'https://pp-api-digitalpassport.hubtotal.net/sso/oauth2/total/connect/jwk_uri',
+    },
+  },
   prod: {
-      // Prod config to be added
+    // Prod config to be added
   },
 };
 export default AUTH_CONFIG;
