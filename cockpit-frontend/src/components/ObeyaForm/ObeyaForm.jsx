7e30cfbb88b078d97fd4784247c5123d486d758e
rihab <rihabrjab08@gmail.com>
2020-07-23 13:59:46 +0200

    feat(team-health): add select team mood

--- cockpit-frontend/src/components/ObeyaForm/ObeyaForm.jsx
+++ cockpit-frontend/src/components/ObeyaForm/ObeyaForm.jsx
@@ -6,48 +6,44 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import FormControl from '@material-ui/core/FormControl';
 
-export default function ObeyaForm(props) {
-    const [list, setList] = useState([{value:1, label:"awesome"},{value:2, label:"good"}]);
+export default function ObeyaForm() {
+  const [list, setList] = useState([
+    { value: 1, label: 'awesome' },
+    { value: 2, label: 'good' },
+  ]);
 
-  const sprint = props.sprintNumber;
-  const teamMotivation=2;
-  const teamConfidence=2;
-  //const list=[{value:1, label:"awesome"},{value:2, label:"good"}];
-  function handleEntityChange(event) {
-      
-   // setTeamMood(event.target.value);
-  }
   const handleSelect = (option) => {
-      debugger
-   setList(list.map(genre => {
-    return {
-      ...genre,
-      selected: genre.value === option
-    };
-  }))
+    debugger;
+    setList(
+      list.map((item) => {
+        return {
+          ...item,
+          // label: `${option}`,
+          selected: item.value === option,
+        };
+      }),
+    );
   };
-  const selected = list.find(genre => genre.selected);
-    const selectedVal = selected ? selected.value : " ";
+  const selected = list.find((item) => item.selected);
+  const selectedVal = selected ? selected.value : ' ';
   return (
     <Grid container spacing={1}>
       <Grid item xs={3}>
         <FormLabel>Mood</FormLabel>
         <FormControl required size="small" fullWidth variant="outlined">
           <Select
-            displayEmpty
-         //   value={teamMood || ''}
-           // onChange={handleEntityChange}
-           value={selectedVal || ''}
-
-           onChange={option => handleSelect(option)}
-           // optionLabelProp={"value"}
+            value={selectedVal}
+            onChange={(e) => handleSelect(e.target.value)}
+            // optionLabelProp={"value"}
           >
-              {list.map((number) => ( <MenuItem value={number.value} key={number.value}>{number.label}</MenuItem> ))}
-            
+            {list.map((item) => (
+              <MenuItem value={item.value} key={item.value}>
+                {item.label}
+              </MenuItem>
+            ))}
           </Select>
         </FormControl>
       </Grid>
-    
     </Grid>
   );
 }
