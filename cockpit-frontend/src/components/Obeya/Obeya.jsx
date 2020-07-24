82128491ae8a402d4e922b142804e8aaba315b0c
rihab <rihabrjab08@gmail.com>
2020-07-24 17:19:35 +0200

    feat(team-health): add text to obeya mvp card

--- cockpit-frontend/src/components/Obeya/Obeya.jsx
+++ cockpit-frontend/src/components/Obeya/Obeya.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import { useLocation } from 'react-router-dom';
 import MvpService from '../../services/apiService';
 import useStyles from './styles';
 import {
@@ -16,6 +17,7 @@ import {
 import { selectedTabState } from '../MvpInfoPage/MvpInfoPageSlice';
 
 export default function Obeya(props) {
+  const isHomePage = useLocation().pathname === '/';
   const classes = useStyles();
   const dispatch = useDispatch();
   const { mvp } = props;
@@ -23,6 +25,7 @@ export default function Obeya(props) {
   const mood = useSelector(moodState);
   const motivation = useSelector(motivationState);
   const confidence = useSelector(confidenceState);
+  const [display, setDisplay] = useState(true);
   useEffect(() => {
     async function getObeya() {
       const sprint = await MvpService.getSprint(
@@ -36,6 +39,7 @@ export default function Obeya(props) {
             sprint.data.teamConfidence) ||
           selectedTab === 'sprint'
         ) {
+          setDisplay(false);
           dispatch(setMood(sprint.data.teamMood));
           dispatch(setMotivation(sprint.data.teamMotivation));
           dispatch(setConfidence(sprint.data.teamConfidence));
@@ -55,52 +59,63 @@ export default function Obeya(props) {
     getObeya();
   }, [dispatch, mvp, selectedTab]);
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={4}>
-        <Typography gutterBottom className={classes.greyTitles}>
-          Mood
-        </Typography>
-        <div>
-          <LinearProgress
-            className={classes.progress}
-            variant="determinate"
-            value={mood * 25}
-          />
-          <span className={classes.progressBarTxt} style={{ left: '5%' }}>
-            {mood || 0}
-          </span>
-        </div>
+    <div>
+      <Grid container spacing={3}>
+        <Grid item xs={4}>
+          <Typography gutterBottom className={classes.greyTitles}>
+            Mood
+          </Typography>
+          <div>
+            <LinearProgress
+              className={classes.progress}
+              variant="determinate"
+              value={mood * 25}
+            />
+            <span className={classes.progressBarTxt} style={{ left: '5%' }}>
+              {mood || 0}
+            </span>
+          </div>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography gutterBottom className={classes.greyTitles}>
+            Confidence
+          </Typography>
+          <div>
+            <LinearProgress
+              className={classes.progress}
+              variant="determinate"
+              value={motivation * 25}
+            />
+            <span className={classes.progressBarTxt} style={{ left: '5%' }}>
+              {motivation || 0}
+            </span>
+          </div>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography gutterBottom className={classes.greyTitles}>
+            Motivation
+          </Typography>
+          <div>
+            <LinearProgress
+              className={classes.progress}
+              variant="determinate"
+              value={confidence * 25}
+            />
+            <span className={classes.progressBarTxt} style={{ left: '5%' }}>
+              {confidence || 0}
+            </span>
+          </div>
+        </Grid>
       </Grid>
-      <Grid item xs={4}>
-        <Typography gutterBottom className={classes.greyTitles}>
-          Confidence
+      {isHomePage && display && (
+        <Typography
+          className={classes.subTitle}
+          color="textSecondary"
+          gutterBottom
+        >
+          Obeya of Last Sprint
         </Typography>
-        <div>
-          <LinearProgress
-            className={classes.progress}
-            variant="determinate"
-            value={motivation * 25}
-          />
-          <span className={classes.progressBarTxt} style={{ left: '5%' }}>
-            {motivation || 0}
-          </span>
-        </div>
-      </Grid>
-      <Grid item xs={4}>
-        <Typography gutterBottom className={classes.greyTitles}>
-          Motivation
-        </Typography>
-        <div>
-          <LinearProgress
-            className={classes.progress}
-            variant="determinate"
-            value={confidence * 25}
-          />
-          <span className={classes.progressBarTxt} style={{ left: '5%' }}>
-            {confidence || 0}
-          </span>
-        </div>
-      </Grid>
-    </Grid>
+      )}
+    </div>
   );
 }
