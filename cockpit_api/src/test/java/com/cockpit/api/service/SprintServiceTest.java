4dcadce296db4d2228932b2a950879ee3796e663
rihab <rihabrjab08@gmail.com>
2020-07-24 16:51:22 +0200

    feat(team-health): add junit tests

--- cockpit_api/src/test/java/com/cockpit/api/service/SprintServiceTest.java
+++ cockpit_api/src/test/java/com/cockpit/api/service/SprintServiceTest.java
@@ -1,5 +1,47 @@
 package com.cockpit.api.service;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.cockpit.api.model.dao.Sprint;
+import com.cockpit.api.model.dto.ObeyaDTO;
+import com.cockpit.api.repository.SprintRepository;
+
+@RunWith(SpringRunner.class)
 public class SprintServiceTest {
+
+	private SprintService sprintService;
+	@MockBean
+	private SprintRepository sprintRepository;
+
+	@Before
+	public void setUp() {
+		this.sprintService = new SprintService(sprintRepository);
+	}
+
+	@Test
+	public void whenAddTeamHealthToSprintThenReturnSprint() {
+
+		ObeyaDTO obeya = new ObeyaDTO(2, 2, 2);
+		Sprint sprint = new Sprint();
+		sprint.setId(1L);
+		// Given
+		Mockito.when(sprintRepository.save(sprint)).thenReturn(sprint);
+		
+		// When
+		sprintService.setTeamHealth(obeya, sprint);
+		
+		// Then
+		Assert.assertEquals(2, (int)(sprint.getTeamMood()));
+		Assert.assertEquals(2, (int)(sprint.getTeamMotivation()));
+		Assert.assertEquals(2, (int)(sprint.getTeamConfidence()));
+
+		
+		}
 
 }
