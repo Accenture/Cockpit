7ca3ac9c7da275cf5d48aaf527970598a25882d3
Zuming <zuming.liu@accenture.com>
2020-07-27 14:10:39 +0200

    feat(digitalpass): add config for digital pass prod

--- cockpit-frontend/src/common/utils/authConfig.js
+++ cockpit-frontend/src/common/utils/authConfig.js
@@ -42,7 +42,26 @@ const AUTH_CONFIG = {
     },
   },
   prod: {
-    // Prod config to be added
+    // Prod config to be updated by Digital Pass Prod
+    client_id: 'cockpit-qa',
+    redirect_uri: 'https://azwbqcokp01.azurewebsites.net/authentication',
+    response_type: 'id_token token',
+    scope: 'openid email',
+    load_user_info: false,
+    authority: 'https://pp-sso-digitalpassport.hubtotal.net/sso/oauth2/total',
+    metadata: {
+      issuer: 'https://pp-sso-digitalpassport.hubtotal.net/sso',
+      end_session_endpoint:
+        'https://pp-sso-digitalpassport.hubtotal.net/sso/oauth2/total/connect/endSession',
+      check_session_iframe:
+        'https://pp-sso-digitalpassport.hubtotal.net/sso/oauth2/total/connect/checkSession',
+      authorization_endpoint:
+        'https://pp-sso-digitalpassport.hubtotal.net/sso/oauth2/total/authorize?usesso=true',
+      userinfo_endpoint:
+        'https://pp-api-digitalpassport.hubtotal.net/sso/oauth2/total/userinfo',
+      jwks_uri:
+        'https://pp-api-digitalpassport.hubtotal.net/sso/oauth2/total/connect/jwk_uri',
+    },
   },
 };
 export default AUTH_CONFIG;
