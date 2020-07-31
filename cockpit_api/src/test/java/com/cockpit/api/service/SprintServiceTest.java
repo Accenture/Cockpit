0f0447de2e75e6f79517f71a9dea6515bde7fa53
rihab <rihabrjab08@gmail.com>
2020-07-31 16:26:01 +0200

    feat(add-impediment): add junit tests

--- cockpit_api/src/test/java/com/cockpit/api/service/SprintServiceTest.java
+++ cockpit_api/src/test/java/com/cockpit/api/service/SprintServiceTest.java
@@ -14,38 +14,59 @@ import com.cockpit.api.model.dao.Sprint;
 import com.cockpit.api.model.dto.ObeyaDTO;
 import com.cockpit.api.repository.SprintRepository;
 
+import java.util.HashSet;
+
 @RunWith(SpringRunner.class)
 public class SprintServiceTest {
 
-	private SprintService sprintService;
-	@MockBean
-	private SprintRepository sprintRepository;
-	@MockBean
-	private ImpedimentRepository impedimentRepository;
+    private SprintService sprintService;
+    @MockBean
+    private SprintRepository sprintRepository;
+    @MockBean
+    private ImpedimentRepository impedimentRepository;
 
-	@Before
-	public void setUp() {
-		this.sprintService = new SprintService(sprintRepository, impedimentRepository);
-	}
+    @Before
+    public void setUp() {
+        this.sprintService = new SprintService(sprintRepository, impedimentRepository);
+    }
 
-	@Test
-	public void whenAddTeamHealthToSprintThenReturnSprint() {
+    @Test
+    public void whenAddTeamHealthToSprintThenReturnSprint() {
 
-		ObeyaDTO obeya = new ObeyaDTO(2, 2, 2);
-		Sprint sprint = new Sprint();
-		sprint.setId(1L);
-		// Given
-		Mockito.when(sprintRepository.save(sprint)).thenReturn(sprint);
-		
-		// When
-		sprintService.setTeamHealth(obeya, sprint);
-		
-		// Then
-		Assert.assertEquals(2, (int)(sprint.getTeamMood()));
-		Assert.assertEquals(2, (int)(sprint.getTeamMotivation()));
-		Assert.assertEquals(2, (int)(sprint.getTeamConfidence()));
+        ObeyaDTO obeya = new ObeyaDTO(2, 2, 2);
+        Sprint sprint = new Sprint();
+        sprint.setId(1L);
+        // Given
+        Mockito.when(sprintRepository.save(sprint)).thenReturn(sprint);
 
-		
-		}
+        // When
+        sprintService.setTeamHealth(obeya, sprint);
+
+        // Then
+        Assert.assertEquals(2, (int) (sprint.getTeamMood()));
+        Assert.assertEquals(2, (int) (sprint.getTeamMotivation()));
+        Assert.assertEquals(2, (int) (sprint.getTeamConfidence()));
+
+
+    }
+
+    @Test
+    public void whenAddImpedimentToSprintThenReturnSprint() {
+
+        Impediment impediment = new Impediment("impediment name", "impediment description");
+        Sprint sprint = new Sprint();
+        sprint.setId(1L);
+        sprint.setImpediments(new HashSet<>());
+        // Given
+        Mockito.when(impedimentRepository.save(impediment)).thenReturn(impediment);
+
+        // When
+        sprintService.addImpediment(impediment, sprint);
+
+        // Then
+        Assert.assertEquals(impediment.getSprint(),sprint);
+
+
+    }
 
 }
