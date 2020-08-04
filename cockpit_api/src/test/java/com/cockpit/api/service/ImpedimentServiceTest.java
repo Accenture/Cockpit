b4b137d03fd9e7dee7a07b73c6f9317096079b85
rihab <rihabrjab08@gmail.com>
2020-08-04 11:39:58 +0200

    feat(delete-impediment): add junit tests

---
+++ cockpit_api/src/test/java/com/cockpit/api/service/ImpedimentServiceTest.java
@@ -0,0 +1,52 @@
+package com.cockpit.api.service;
+
+import com.cockpit.api.exception.ResourceNotFoundException;
+import com.cockpit.api.model.dao.Impediment;
+import com.cockpit.api.model.dao.Sprint;
+import com.cockpit.api.repository.ImpedimentRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashSet;
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+public class ImpedimentServiceTest {
+
+    private ImpedimentService impedimentService;
+
+    @MockBean
+    private ImpedimentRepository impedimentRepository;
+
+
+    @Before
+    public void setUp() {
+        this.impedimentService = new ImpedimentService(impedimentRepository);
+    }
+
+    @Test
+    public void whenDeleteImpedimentThenDeleteImpedimentFromSprint() throws ResourceNotFoundException {
+        Impediment mockImpediment = new Impediment("impediment name", "impediment description");
+        mockImpediment.setId(1L);
+        Sprint sprint = new Sprint();
+        sprint.setId(1L);
+        sprint.setImpediments(new HashSet<>());
+        mockImpediment.setSprint(sprint);
+        Optional<Impediment> impediment = Optional.ofNullable(mockImpediment);
+
+        // Given
+        Mockito.when(impedimentRepository.findById(mockImpediment.getId())).thenReturn(impediment);
+
+        // When
+        impedimentService.deleteImpediment(mockImpediment.getId());
+
+        // Then
+        Assert.assertTrue(sprint.getImpediments().isEmpty());
+    }
+
+}
