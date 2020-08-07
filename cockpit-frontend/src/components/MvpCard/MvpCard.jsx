c7b79b70e2e53c8b94628806447e7b695228802b
rihab <rihabrjab08@gmail.com>
2020-08-07 09:43:44 +0200

    fix(cycle): remove cycle from frontend

--- cockpit-frontend/src/components/MvpCard/MvpCard.jsx
+++ cockpit-frontend/src/components/MvpCard/MvpCard.jsx
@@ -47,7 +47,7 @@ export default function MvpCard(props) {
           </Grid>
         )}
         <Grid container spacing={3}>
-          <Grid item xs={6}>
+          <Grid item xs={12}>
             <Typography gutterBottom>
               {mvpInfo.jira.currentSprint
                 ? mvpInfo.jira.currentSprint
