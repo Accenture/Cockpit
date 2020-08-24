e2f571bc2f4234bca6d60af8f51d62da67c212c1
rihab <rihabrjab08@gmail.com>
2020-08-24 15:38:41 +0200

    feat(team-name): add control to team name

--- cockpit_api/src/main/java/com/cockpit/api/controller/TeamController.java
+++ cockpit_api/src/main/java/com/cockpit/api/controller/TeamController.java
@@ -19,120 +19,125 @@ import com.cockpit.api.service.TeamService;
 @RestController
 @CrossOrigin
 public class TeamController {
-	private final TeamService teamService;
-	private final AuthService authService;
+    private final TeamService teamService;
+    private final AuthService authService;
 
-	@Autowired
-	public TeamController(TeamService teamService, AuthService authService) {
-		this.teamService = teamService;
-		this.authService = authService;
-	}
+    @Autowired
+    public TeamController(TeamService teamService, AuthService authService) {
+        this.teamService = teamService;
+        this.authService = authService;
+    }
 
-	// CREATE a new Team and assign it to an MVP
-	@PostMapping(value = "/api/v1/team/create/{mvpId}")
-	public ResponseEntity createTeam(@PathVariable Long mvpId,
-									 @RequestBody TeamDTO teamDTO,
-									 @RequestHeader("Authorization") String authHeader)
-			throws ResourceNotFoundException {
-		if (authService.isUserAuthorized(authHeader)) {
-			TeamDTO newTeam = teamService.createNewTeam(teamDTO, mvpId);
-			return ResponseEntity.ok(newTeam);
-		} else {
-			return ResponseEntity.ok(HttpStatus.UNAUTHORIZED);
-		}
-	}
+    // CREATE a new Team and assign it to an MVP
+    @PostMapping(value = "/api/v1/team/create/{mvpId}")
+    public ResponseEntity createTeam(@PathVariable Long mvpId,
+                                     @RequestBody TeamDTO teamDTO,
+                                     @RequestHeader("Authorization") String authHeader)
+            throws ResourceNotFoundException {
+        if (authService.isUserAuthorized(authHeader)) {
+            try {
+                TeamDTO newTeam = teamService.createNewTeam(teamDTO, mvpId);
+                return ResponseEntity.ok(newTeam);
 
-	// GET ALL
-	@GetMapping(value = "/api/v1/team/all")
-	public ResponseEntity getTeams(@RequestHeader("Authorization") String authHeader) {
-		if (authService.isUserAuthorized(authHeader)) {
-			List<TeamDTO> mvpList = teamService.findAll();
-			return ResponseEntity.ok(mvpList);
-		} else {
-			return ResponseEntity.ok(HttpStatus.UNAUTHORIZED);
-		}
-	}
+            } catch (Exception e) {
+                return ResponseEntity.ok().body(null);
+            }
+        } else {
+            return ResponseEntity.ok(HttpStatus.UNAUTHORIZED);
+        }
+    }
 
-	// GET Team BY ID
-	@GetMapping(value = "/api/v1/team/{id}")
-	public ResponseEntity getTeamById(@PathVariable Long id,
-									  @RequestHeader("Authorization") String authHeader) {
-		if (authService.isUserAuthorized(authHeader)) {
-			try {
-				TeamDTO teamFound = teamService.findTeamById(id);
-				return ResponseEntity.ok().body(teamFound);
-			} catch (com.cockpit.api.exception.ResourceNotFoundException e) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-			}
-		} else {
-			return ResponseEntity.ok(HttpStatus.UNAUTHORIZED);
-		}
-	}
+    // GET ALL
+    @GetMapping(value = "/api/v1/team/all")
+    public ResponseEntity getTeams(@RequestHeader("Authorization") String authHeader) {
+        if (authService.isUserAuthorized(authHeader)) {
+            List<TeamDTO> mvpList = teamService.findAll();
+            return ResponseEntity.ok(mvpList);
+        } else {
+            return ResponseEntity.ok(HttpStatus.UNAUTHORIZED);
+        }
+    }
 
-	// UPDATE a Team
-	@PutMapping(value = "/api/v1/team/update/{id}")
-	public ResponseEntity updateTeam(@RequestBody TeamDTO teamDTO,
-									 @PathVariable Long id,
-									 @RequestHeader("Authorization") String authHeader) {
-		if (authService.isUserAuthorized(authHeader)) {
-			try {
-				TeamDTO teamUpdated = teamService.updateTeam(teamDTO, id);
-				return ResponseEntity.ok().body(teamUpdated);
-			} catch (com.cockpit.api.exception.ResourceNotFoundException e) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-			}
-		} else {
-			return ResponseEntity.ok(HttpStatus.UNAUTHORIZED);
-		}
-	}
+    // GET Team BY ID
+    @GetMapping(value = "/api/v1/team/{id}")
+    public ResponseEntity getTeamById(@PathVariable Long id,
+                                      @RequestHeader("Authorization") String authHeader) {
+        if (authService.isUserAuthorized(authHeader)) {
+            try {
+                TeamDTO teamFound = teamService.findTeamById(id);
+                return ResponseEntity.ok().body(teamFound);
+            } catch (com.cockpit.api.exception.ResourceNotFoundException e) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+            }
+        } else {
+            return ResponseEntity.ok(HttpStatus.UNAUTHORIZED);
+        }
+    }
 
-	// DELETE a Team
-	@DeleteMapping(value = "/api/v1/team/delete/{id}")
-	public ResponseEntity deleteTeam(@PathVariable Long id,
-											 @RequestHeader("Authorization") String authHeader) {
-		if (authService.isUserAuthorized(authHeader)) {
-			try {
-				teamService.deleteTeam(id);
-				return ResponseEntity.ok("One Team has been deleted");
-			} catch (ResourceNotFoundException e) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-			}
-		} else {
-			return ResponseEntity.ok(HttpStatus.UNAUTHORIZED);
-		}
-	}
+    // UPDATE a Team
+    @PutMapping(value = "/api/v1/team/update/{id}")
+    public ResponseEntity updateTeam(@RequestBody TeamDTO teamDTO,
+                                     @PathVariable Long id,
+                                     @RequestHeader("Authorization") String authHeader) {
+        if (authService.isUserAuthorized(authHeader)) {
+            try {
+                TeamDTO teamUpdated = teamService.updateTeam(teamDTO, id);
+                return ResponseEntity.ok().body(teamUpdated);
+            } catch (com.cockpit.api.exception.ResourceNotFoundException e) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+            }
+        } else {
+            return ResponseEntity.ok(HttpStatus.UNAUTHORIZED);
+        }
+    }
 
-	// ADD a Team member
-	@PutMapping(value = "/api/v1/team/addTeamMember/{id}")
-	public ResponseEntity addTeamMember(@RequestBody TeamMemberDTO teamMemberDTO,
-										@PathVariable Long id,
-										@RequestHeader("Authorization") String authHeader) {
-		if (authService.isUserAuthorized(authHeader)) {
-			try {
-				TeamDTO teamUpdated = teamService.createTeamMember(id, teamMemberDTO);
-				return ResponseEntity.ok().body(teamUpdated);
-			} catch (com.cockpit.api.exception.ResourceNotFoundException e) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-			}
-		} else {
-			return ResponseEntity.ok(HttpStatus.UNAUTHORIZED);
-		}
-	}
+    // DELETE a Team
+    @DeleteMapping(value = "/api/v1/team/delete/{id}")
+    public ResponseEntity deleteTeam(@PathVariable Long id,
+                                     @RequestHeader("Authorization") String authHeader) {
+        if (authService.isUserAuthorized(authHeader)) {
+            try {
+                teamService.deleteTeam(id);
+                return ResponseEntity.ok("One Team has been deleted");
+            } catch (ResourceNotFoundException e) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+            }
+        } else {
+            return ResponseEntity.ok(HttpStatus.UNAUTHORIZED);
+        }
+    }
 
-	// DELETE a Team member
-	@PutMapping(value = "/api/v1/team/{id}/deleteTeamMember/{teamMemberId}")
-	public ResponseEntity deleteTeamMember(@PathVariable("id") Long id,
-										   @PathVariable("teamMemberId") Long teamMemberId,
-										   @RequestHeader("Authorization") String authHeader) {
-		if (authService.isUserAuthorized(authHeader)) {
-			try {
-				TeamDTO teamUpdated = teamService.deleteTeamMember(id, teamMemberId);
-				return ResponseEntity.ok().body(teamUpdated);
-			} catch (com.cockpit.api.exception.ResourceNotFoundException e) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-			}
-		} else {
-			return ResponseEntity.ok(HttpStatus.UNAUTHORIZED);
-		}
-	}
+    // ADD a Team member
+    @PutMapping(value = "/api/v1/team/addTeamMember/{id}")
+    public ResponseEntity addTeamMember(@RequestBody TeamMemberDTO teamMemberDTO,
+                                        @PathVariable Long id,
+                                        @RequestHeader("Authorization") String authHeader) {
+        if (authService.isUserAuthorized(authHeader)) {
+            try {
+                TeamDTO teamUpdated = teamService.createTeamMember(id, teamMemberDTO);
+                return ResponseEntity.ok().body(teamUpdated);
+            } catch (com.cockpit.api.exception.ResourceNotFoundException e) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+            }
+        } else {
+            return ResponseEntity.ok(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    // DELETE a Team member
+    @PutMapping(value = "/api/v1/team/{id}/deleteTeamMember/{teamMemberId}")
+    public ResponseEntity deleteTeamMember(@PathVariable("id") Long id,
+                                           @PathVariable("teamMemberId") Long teamMemberId,
+                                           @RequestHeader("Authorization") String authHeader) {
+        if (authService.isUserAuthorized(authHeader)) {
+            try {
+                TeamDTO teamUpdated = teamService.deleteTeamMember(id, teamMemberId);
+                return ResponseEntity.ok().body(teamUpdated);
+            } catch (com.cockpit.api.exception.ResourceNotFoundException e) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+            }
+        } else {
+            return ResponseEntity.ok(HttpStatus.UNAUTHORIZED);
+        }
+    }
 }
