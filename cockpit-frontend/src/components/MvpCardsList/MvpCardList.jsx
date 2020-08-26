914fb037ffdb5e29fba5d9857b426ea4635cdf48
rihab <rihabrjab08@gmail.com>
2020-08-26 11:36:36 +0200

    refactor(delete-mvp): add confirm popup before delete mvp

--- cockpit-frontend/src/components/MvpCardsList/MvpCardList.jsx
+++ cockpit-frontend/src/components/MvpCardsList/MvpCardList.jsx
@@ -4,16 +4,22 @@ import Grid from '@material-ui/core/Grid';
 import { Link } from 'react-router-dom';
 import DeleteIcon from '@material-ui/icons/Delete';
 import IconButton from '@material-ui/core/IconButton';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import Button from '@material-ui/core/Button';
+import MvpCard from '../MvpCard/MvpCard';
 import { fetchAllMvps, deleteMvp } from '../../redux/ormSlice';
 import { mvpSelector } from '../../redux/selector';
-import MvpCard from '../MvpCard/MvpCard';
-
 // styles
 import useStyles from './styles';
 
 export default function MvpCardList(props) {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const [openDialog, setOpenDialog] = React.useState(false);
+  const [selectedMvp, setSelectedMvp] = React.useState({});
 
   useEffect(() => {
     dispatch(fetchAllMvps());
@@ -27,7 +33,16 @@ export default function MvpCardList(props) {
   function deleteJira(e, id) {
     e.stopPropagation();
     dispatch(deleteMvp(id));
+    setOpenDialog(false);
   }
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+  const handleClickOpen = (mvp) => {
+    setOpenDialog(true);
+    setSelectedMvp(mvp);
+  };
+
   return (
     <Grid container>
       <Grid item xs={12}>
@@ -38,7 +53,7 @@ export default function MvpCardList(props) {
                 className={classes.deleteButton}
                 aria-label="delete"
                 color="secondary"
-                onClick={(e) => deleteJira(e, mvp.id)}
+                onClick={() => handleClickOpen(mvp)}
               >
                 <DeleteIcon />
               </IconButton>
@@ -52,6 +67,24 @@ export default function MvpCardList(props) {
           ))}
         </Grid>
       </Grid>
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to delete this mvp definitely?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={(e) => deleteJira(e, selectedMvp.id)}
+            color="primary"
+          >
+            Yes
+          </Button>
+          <Button onClick={handleCloseDialog} color="primary">
+            No
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Grid>
   );
 }
