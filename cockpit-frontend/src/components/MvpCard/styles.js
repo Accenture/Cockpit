fa152d0f4330e5452bdcd006bc696d2dfb82fcd2
Zuming <zuming.liu@accenture.com>
2020-08-31 17:54:51 +0200

    refactor(css): refactor margin for cards

--- cockpit-frontend/src/components/MvpCard/styles.js
+++ cockpit-frontend/src/components/MvpCard/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
   dashboardCard: {
-    margin: 'auto',
+    marginTop: '-48px',
     padding: theme.spacing(2),
   },
   mvpInfoCard: {
