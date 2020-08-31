85e9ce8ca65cb525002f113b66a5fb91f74e6a27
Zuming <zuming.liu@accenture.com>
2020-08-31 17:54:51 +0200

    refactor(css): refactor margin for cards

--- cockpit-frontend/src/components/MvpTabPanel/styles.js
+++ cockpit-frontend/src/components/MvpTabPanel/styles.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    marginTop: '48px',
     backgroundColor: theme.palette.background.paper,
   },
 }));
