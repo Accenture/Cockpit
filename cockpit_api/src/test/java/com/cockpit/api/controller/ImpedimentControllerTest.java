08cb36ce7535f0f5050505da163f036459381d7c
rihab <rihabrjab08@gmail.com>
2020-08-31 12:13:04 +0200

    feat(update-impediment): add junit update impediment controller test

--- cockpit_api/src/test/java/com/cockpit/api/controller/ImpedimentControllerTest.java
+++ cockpit_api/src/test/java/com/cockpit/api/controller/ImpedimentControllerTest.java
@@ -1,11 +1,14 @@
 package com.cockpit.api.controller;
 
 import com.cockpit.api.model.dao.Impediment;
+import com.cockpit.api.model.dto.ImpedimentDTO;
 import com.cockpit.api.service.AuthService;
 import com.cockpit.api.service.ImpedimentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -25,6 +28,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ContextConfiguration(classes = {ImpedimentController.class})
 @WebMvcTest
 public class ImpedimentControllerTest {
+
+    private ModelMapper modelMapper = new ModelMapper();
 
     @Autowired
     private MockMvc mockMvc;
@@ -48,6 +53,30 @@ public class ImpedimentControllerTest {
                 .perform(MockMvcRequestBuilders.delete("/api/v1/impediment/delete/{id}", mockImpediment.getId())
                         .accept(MediaType.APPLICATION_JSON)
                         .header("Authorization", "Bearer token"))
+                .andExpect(status().isOk()).andReturn();
+
+        MockHttpServletResponse response = result.getResponse();
+
+        // then
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
+
+    @Test
+    public void whenUpdateImpedimentThenReturn200() throws Exception {
+        Impediment mockImpediment = new Impediment("new name", "new description");
+        mockImpediment.setId(1L);
+        ImpedimentDTO impedimentDTO = modelMapper.map(mockImpediment, ImpedimentDTO.class);
+
+        // given
+        Mockito.when(impedimentService.updateImpediment(impedimentDTO, mockImpediment.getId())).thenReturn(impedimentDTO);
+
+        // when
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.put("/api/v1/impediment/update/{id}", mockImpediment.getId())
+                        .header("Authorization", "Bearer token")
+                        .content(new ObjectMapper().writeValueAsString(impedimentDTO))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk()).andReturn();
 
         MockHttpServletResponse response = result.getResponse();
