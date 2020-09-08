8d5056a9be53c9df47dbb9a9c64e1bdb40a2e023
Zuming <zuming.liu@accenture.com>
2020-09-08 16:23:28 +0200

    fix(auth): return is scrum master as true locally

--- cockpit_api/src/main/java/com/cockpit/api/service/AuthService.java
+++ cockpit_api/src/main/java/com/cockpit/api/service/AuthService.java
@@ -42,7 +42,7 @@ public class AuthService {
         HttpHeaders headers = addAuthorizationToHeaders(authHeader);
         HttpEntity<String> request = new HttpEntity<>(headers);
         if (isAuthEnabled.equals("false")) {
-            return false;
+            return true;
         } else {
             try {
                 ResponseEntity<UserInfoDTO> responseEntity = restTemplate.exchange(userInfoEndpoint, HttpMethod.POST, request, UserInfoDTO.class);
