31d4a1b61627228acfcacb5dabb6131003afdb18
rihab <rihabrjab08@gmail.com>
2020-09-08 16:16:10 +0200

    feat(select-existing-member): add assign button

--- cockpit-frontend/src/components/ExistingTeamMembers/ExisitingTeamMmebers.jsx
+++ cockpit-frontend/src/components/ExistingTeamMembers/ExisitingTeamMmebers.jsx
@@ -1,40 +1,89 @@
 import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-// import { makeStyles } from '@material-ui/core/styles';
+import { mvpSelector } from '../../redux/selector';
+import useStyles from './styles';
 import MvpService from '../../services/apiService';
 
 export default function ExisitingTeamMmebers() {
   const [members, setMembers] = React.useState([]);
+  const [selectedMember, setSelectedMember] = React.useState(null);
+  const mvpId = useParams().id;
+  const mvpInfo = useSelector((state) => mvpSelector(state, mvpId));
+  const classes = useStyles();
 
   useEffect(() => {
     async function getAllMembers() {
-      const list = await MvpService.getMembers();
-      setMembers(list.data);
+      const result = await MvpService.getMembers();
+      const list = result.data.filter(
+        (member) =>
+          !mvpInfo.team.teamMembers.find((element) => element.id === member.id),
+      );
+      setMembers(list);
     }
     getAllMembers();
   });
+  function handleSelect(value) {
+    setSelectedMember(value);
+  }
+  function assign() {}
   return (
     <div>
       <Autocomplete
         id="members"
         options={members}
         getOptionLabel={(option) => option.email}
-        style={{ width: 300 }}
+        style={{ width: 275 }}
+        onChange={(event, value) => handleSelect(value)}
         renderInput={(params) => (
           <TextField {...params} label="Member Email" variant="outlined" />
         )}
+        getOptionSelected={(option, value) => option.email === value.email}
         renderOption={(option) => (
           <>
-            <span>{option.email}</span>({option.firstName}) {option.lastName}
+            <Grid container spacing={1}>
+              <Grid item xs={6}>
+                <Avatar alt="team member" src={option.urlTeamMemberAvatar} />
+              </Grid>
+              <Grid item xs={6}>
+                <Typography
+                  component="span"
+                  variant="body2"
+                  color="textPrimary"
+                >
+                  {option.firstName} {option.lastName}
+                </Typography>
+                <br />
+                <Typography
+                  component="span"
+                  variant="body2"
+                  color="textSecondary"
+                >
+                  {option.role}
+                </Typography>
+              </Grid>
+            </Grid>
           </>
         )}
       />
+      <Button
+        onClick={assign}
+        id="submit"
+        value="Submit"
+        type="submit"
+        disabled={selectedMember === null}
+        variant="outlined"
+        color="primary"
+        className={classes.buttonAssign}
+      >
+        assign
+      </Button>
     </div>
   );
 }
