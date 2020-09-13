5a63a53b0df1a9194828686b5dfa7b15a9a867ca
Zuming <zuming.liu@accenture.com>
2020-09-14 00:44:47 +0200

    test(coverage): add unit test for scheduled tasks update jira

---
+++ cockpit_api/src/test/java/com/cockpit/api/service/jiragateway/JiraApiServiceTest.java
@@ -0,0 +1,45 @@
+package com.cockpit.api.service.jiragateway;
+
+import com.cockpit.api.exception.JiraException;
+import com.cockpit.api.model.dto.jira.Project;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = JiraApiService.class)
+public class JiraApiServiceTest {
+
+    @Autowired
+    private JiraApiService jiraApiService;
+
+    @Value("${spring.jira.username}")
+    private String username;
+    @Value("${spring.jira.token}")
+    private String token;
+    @Value("${spring.jira.urlProjects}")
+    private String urlProjects;
+
+    @Before
+    public void setUp() {
+        this.jiraApiService = new JiraApiService();
+        ReflectionTestUtils.setField(jiraApiService, "username", username);
+        ReflectionTestUtils.setField(jiraApiService, "token", token);
+    }
+
+    @Test
+    public void whenCallingJiraUrlThenReturn200() throws JiraException {
+        ResponseEntity response = jiraApiService.callJira(urlProjects, Project[].class.getName());
+        assertEquals(HttpStatus.OK.value(), response.getStatusCodeValue());
+    }
+
+}
---
+++ cockpit_api/src/test/java/com/cockpit/api/service/jiragateway/UpdateJiraTest.java
@@ -0,0 +1,122 @@
+package com.cockpit.api.service.jiragateway;
+
+import com.cockpit.api.exception.JiraException;
+import com.cockpit.api.model.dao.Jira;
+import com.cockpit.api.model.dto.jira.Board;
+import com.cockpit.api.model.dto.jira.JiraBoard;
+import com.cockpit.api.model.dto.jira.Location;
+import com.cockpit.api.model.dto.jira.Project;
+import com.cockpit.api.repository.JiraRepository;
+import com.cockpit.api.service.UserStoryService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UpdateJiraTest {
+
+    private UpdateJira updateJira;
+
+    @MockBean
+    private JiraApiService jiraApiService;
+
+    @Autowired
+    private UserStoryService userStoryService;
+
+    @MockBean
+    private JiraRepository jiraRepository;
+
+
+    @Value("${spring.jira.urlProjects}")
+    private String urlProjects;
+    @Value("${spring.jira.urlBoards}")
+    private String urlBoards;
+
+    @Before
+    public void setUp() {
+        this.updateJira = new UpdateJira(jiraRepository, jiraApiService, userStoryService);
+        ReflectionTestUtils.setField(updateJira, "urlProjects", urlProjects);
+        ReflectionTestUtils.setField(updateJira, "urlBoards", urlBoards);
+    }
+
+    @Test
+    public void whenUpdateJiraProjectIdThenProjectIdUpdated() throws JiraException {
+
+        Project mockProject = new Project();
+        mockProject.setId("100");
+        mockProject.setKey("TEST_KEY");
+        Project[] mockProjectList = new Project[1];
+        mockProjectList[0] = mockProject;
+        ResponseEntity mockResponse = new ResponseEntity(mockProjectList,HttpStatus.OK);
+
+        Jira mockJira = new Jira();
+        mockJira.setJiraProjectKey("TEST_KEY");
+        List<Jira> mockJiraList = new ArrayList<>();
+        mockJiraList.add(mockJira);
+
+        // given
+        Mockito.when(jiraApiService.callJira(urlProjects, Project[].class.getName())).thenReturn(mockResponse);
+        Mockito.when(jiraRepository.findAllByOrderById()).thenReturn(mockJiraList);
+
+        // when
+        updateJira.updateProjectIdInJira();
+
+        // then
+        verify(jiraRepository, atLeastOnce()).save(mockJira);
+
+    }
+
+    @Test
+    public void whenUpdateJiraBoardIdThenBoardIdUpdated() throws JiraException {
+        List<JiraBoard> boardList = new ArrayList<>();
+        JiraBoard mockJiraBoard = new JiraBoard();
+        Location mockLocation = new Location();
+        mockLocation.setProjectId(1000);
+        mockJiraBoard.setLocation(mockLocation);
+        boardList.add(mockJiraBoard);
+
+        Board mockBoard = new Board();
+        mockBoard.setStartAt(0);
+        mockBoard.setTotal(1);
+        mockBoard.setMaxResults(1);
+        mockBoard.setValues(boardList);
+
+        ResponseEntity mockResponse = new ResponseEntity(mockBoard,HttpStatus.OK);
+
+        Jira mockJira = new Jira();
+        mockJira.setJiraProjectKey("TEST_KEY");
+        mockJira.setJiraProjectId(10);
+        mockJira.setBoardId(1000);
+
+        int maxResults = 50;
+        int startAt = 0;
+        String url = String.format(urlBoards, maxResults, startAt);
+
+        // given
+        Mockito.when(jiraApiService.callJira(url, Board.class.getName())).thenReturn(mockResponse);
+        Mockito.when(jiraRepository.findByJiraProjectId(Mockito.anyInt())).thenReturn(mockJira);
+
+        // when
+        updateJira.updateBoardIdInJira();
+
+        // then
+        verify(jiraRepository, atLeastOnce()).findByJiraProjectId(Mockito.anyInt());
+    }
+
+}
