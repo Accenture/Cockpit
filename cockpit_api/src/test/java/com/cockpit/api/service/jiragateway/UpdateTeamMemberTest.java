5f1ac4daad446a6f5e43e66abee7e0541f28c488
Zuming <zuming.liu@accenture.com>
2020-09-16 14:47:37 +0200

    test(coverage): add test for team member scheduled task

---
+++ cockpit_api/src/test/java/com/cockpit/api/service/jiragateway/UpdateTeamMemberTest.java
@@ -0,0 +1,81 @@
+package com.cockpit.api.service.jiragateway;
+
+import com.cockpit.api.exception.JiraException;
+import com.cockpit.api.model.dao.Jira;
+import com.cockpit.api.model.dao.Sprint;
+import com.cockpit.api.model.dao.TeamMember;
+import com.cockpit.api.model.dao.UserStory;
+import com.cockpit.api.model.dto.jira.*;
+import com.cockpit.api.repository.JiraRepository;
+import com.cockpit.api.repository.SprintRepository;
+import com.cockpit.api.repository.TeamMemberRepository;
+import com.cockpit.api.repository.UserStoryRepository;
+import com.cockpit.api.service.UserStoryService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UpdateTeamMemberTest {
+
+    private UpdateTeamMember updateTeamMember;
+
+    @MockBean
+    private JiraApiService jiraApiService;
+
+    @MockBean
+    private TeamMemberRepository teamMemberRepository;
+
+    @Value("${spring.jira.urlUserInformation}")
+    private String urlUserInformation;
+
+    @Before
+    public void setUp() {
+        this.updateTeamMember = new UpdateTeamMember(
+                teamMemberRepository,
+                jiraApiService
+        );
+        ReflectionTestUtils.setField(updateTeamMember, "urlUserInformation", urlUserInformation); }
+
+    @Test
+    public void whenUpdateTeamMemberThenTeamMemberUpdated() throws JiraException {
+
+        User mockUser = new User();
+        AvatarUrls mockAvatarUrls = new AvatarUrls();
+        mockAvatarUrls.setBigAvatar("https://test.com");
+        mockUser.setAvatarUrls(mockAvatarUrls);
+        User[] users = new User[1];
+        users[0] = mockUser;
+        ResponseEntity mockResponse = new ResponseEntity(users,HttpStatus.OK);
+
+        TeamMember mockTeamMember = new TeamMember();
+        mockTeamMember.setId(1l);
+        mockTeamMember.setEmail("test@cockpit.com");
+        List<TeamMember> mockTeamMemberList = new ArrayList<>();
+        mockTeamMemberList.add(mockTeamMember);
+        // given
+        Mockito.when(jiraApiService.callJira(urlUserInformation + "test@cockpit.com", User[].class.getName())).thenReturn(mockResponse);
+        Mockito.when(teamMemberRepository.findAllByOrderById()).thenReturn(mockTeamMemberList);
+
+        // when
+        updateTeamMember.updateTeamMembers();
+
+        // then
+        verify(teamMemberRepository, atLeastOnce()).save(Mockito.any(TeamMember.class));
+
+    }
+}
