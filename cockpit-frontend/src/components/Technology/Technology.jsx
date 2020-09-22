fee4c820b8ccc81abfa0d56f8990f6981e6d35b5
rihab <rihabrjab08@gmail.com>
2020-09-22 17:25:36 +0200

    feat(select-existing-techno): add autocomplete

--- cockpit-frontend/src/components/Technology/Technology.jsx
+++ cockpit-frontend/src/components/Technology/Technology.jsx
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import FormLabel from '@material-ui/core/FormLabel';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { mvpSelector } from '../../redux/selector';
@@ -19,12 +20,25 @@ export default function Technology() {
   const [technoName, setTechnoName] = React.useState('');
   const [technoLogo, setTechnoLogo] = React.useState('');
   const [technologies, setTechnologies] = React.useState([]);
+  const [allTechnologies, setAllTechnologies] = React.useState([]);
   const mvpId = useParams().id;
   const mvpInfo = useSelector((state) => mvpSelector(state, mvpId));
+  const [value, setValue] = React.useState([]);
+
+  useEffect(() => {
+    async function getAllTechnologies() {
+      const result = await MvpService.getAllTechnologies();
+      setAllTechnologies(result.data);
+    }
+    getAllTechnologies();
+  }, []);
 
   useEffect(() => {
     setTechnologies(mvpInfo.technologies);
   }, [mvpInfo]);
+  useEffect(() => {
+    setValue(technologies);
+  }, [technologies]);
   function handleLogoChange(event) {
     setTechnoLogo(event.target.value);
   }
@@ -53,41 +67,101 @@ export default function Technology() {
         urlImage.startsWith('https://'))
     );
   }
+  const onTagsChange = (event, values) => {
+    setValue(values);
+  };
   return (
-    <Paper className={classes.paper}>
-      <div className={classes.technoList}>
-        {technologies.length > 0 &&
-          technologies.map((techno) => (
-            <div key={techno.id}>
-              <span>{techno.name}</span>
-              <br />
-            </div>
-          ))}
-      </div>
-      <Button
-        onClick={() => setOpen(true)}
-        variant="outlined"
-        color="primary"
-        className={classes.buttonSave}
-      >
-        + add new Technology
-      </Button>
-      {open && (
-        <form>
-          <Grid container className={classes.containerAdd}>
-            <Grid item xs={8}>
-              <FormLabel className={classes.formLabel}>Name</FormLabel>
-              <TextField
-                className={classes.textField}
-                required
-                fullWidth
-                variant="outlined"
-                name="name"
-                placeholder="Technology Name"
-                size="small"
-                value={technoName || ''}
-                onChange={handleNameChange}
-              />
+    <div>
+      <Paper className={classes.paper}>
+        <Autocomplete
+          className={classes.technoList}
+          multiple
+          options={allTechnologies}
+          getOptionLabel={(option) => option.name}
+          value={value}
+          onChange={onTagsChange}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              variant="outlined"
+              label="Technology"
+              placeholder="name"
+            />
+          )}
+        />
+        <Button
+          onClick={() => setOpen(true)}
+          variant="outlined"
+          color="primary"
+          className={classes.buttonSave}
+        >
+          + add new Technology
+        </Button>
+        {open && (
+          <form>
+            <Grid container className={classes.containerAdd}>
+              <Grid item xs={8}>
+                <FormLabel className={classes.formLabel}>Name</FormLabel>
+                <TextField
+                  className={classes.textField}
+                  required
+                  fullWidth
+                  variant="outlined"
+                  name="name"
+                  placeholder="Technology Name"
+                  size="small"
+                  value={technoName || ''}
+                  onChange={handleNameChange}
+                />
+              </Grid>
+              <Grid item xs={8}>
+                <FormLabel className={classes.formLabel}>Logo</FormLabel>
+                <TextField
+                  className={classes.textField}
+                  required
+                  fullWidth
+                  variant="outlined"
+                  name="logo"
+                  size="small"
+                  value={technoLogo || ''}
+                  onChange={handleLogoChange}
+                  placeholder="https://..."
+                  error={!isImageUrlValid(technoLogo)}
+                  helperText={
+                    isImageUrlValid(technoLogo)
+                      ? ''
+                      : 'Url starts with http(s)://'
+                  }
+                />
+              </Grid>
+              <Grid item xs={4} />
+              <Grid item xs={2} />
+              <Grid item xs={3}>
+                <Button
+                  onClick={closeForm}
+                  variant="outlined"
+                  color="primary"
+                  className={classes.buttonSave}
+                >
+                  cancel
+                </Button>
+              </Grid>
+              <Grid item xs={3}>
+                <Button
+                  onClick={save}
+                  disabled={
+                    technoName === '' ||
+                    technoLogo === '' ||
+                    !isImageUrlValid(technoLogo)
+                  }
+                  variant="contained"
+                  color="primary"
+                  className={classes.buttonSave}
+                >
+                  add
+                </Button>
+              </Grid>{' '}
+              <Grid item xs={2} />
             </Grid>
             <Grid item xs={8}>
               <FormLabel className={classes.formLabel}>Logo</FormLabel>
