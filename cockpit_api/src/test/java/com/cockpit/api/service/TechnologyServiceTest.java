fc31f52a8e0b5d9ebc25ab22f8b1e63b1c5d8bb1
rihab <rihabrjab08@gmail.com>
2020-09-23 11:19:24 +0200

    feat(remove-techno): add junit tests

--- cockpit_api/src/test/java/com/cockpit/api/service/TechnologyServiceTest.java
+++ cockpit_api/src/test/java/com/cockpit/api/service/TechnologyServiceTest.java
@@ -76,4 +76,29 @@ public class TechnologyServiceTest {
         Assert.assertTrue(!mockMvp.getTechnologies().isEmpty());
 
     }
+    @Test
+    public void whenUnassignTechnologyThenReturnAssignedTechnology() throws ResourceNotFoundException {
+        Mvp mockMvp = new Mvp();
+        mockMvp.setId(1l);
+        mockMvp.setTechnologies(new HashSet<>());
+        Technology mockTechnology = new Technology();
+        mockTechnology.setId(1L);
+        mockTechnology.setName("JAVA");
+        mockTechnology.setUrl("https://www.tc-web.it/wp-content/uploads/2019/12/java.jpg");
+        mockMvp.getTechnologies().add(mockTechnology);
+
+        Optional<Mvp> mvp = Optional.ofNullable(mockMvp);
+        Optional<Technology> technology = Optional.ofNullable(mockTechnology);
+
+        // given
+        Mockito.when(mvpRepository.findById(mockMvp.getId())).thenReturn(mvp);
+        Mockito.when(technologyRepository.findById(mockTechnology.getId())).thenReturn(technology);
+
+        // when
+        technologyService.unassignTechnology(mockTechnology.getId(), mockMvp.getId());
+
+        // then
+        Assert.assertTrue(mockMvp.getTechnologies().isEmpty());
+
+    }
 }
